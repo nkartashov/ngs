76c327f7821b4b27e038ba75530ff7746cef71dc
Nikita Kartashov <snailandmail@gmail.com>
2015-02-25 16:26:22 +0300

    Added hw1/2

--- src/hw1/hw1.py
+++ src/hw1/hw1.py
@@ -55,20 +55,42 @@ def is_read_low_quality(read_quality):
     return sum(list(map(mapper, read_quality))) * 1.0 / len(read_quality) < 0.75
 
 
+def get_record_quality(record):
+    return record.letter_annotations["phred_quality"]
+
+
 def get_gc_content(records):
     # Precision is 0.1
     BINS = 100 * 10
-    gc_contents = [GC(r.seq) for r in records if not is_read_low_quality(r.letter_annotations["phred_quality"])]
+    gc_contents = [GC(r.seq) for r in records if not is_read_low_quality(get_record_quality(r))]
     plt.hist(gc_contents, BINS)
     plt.ylabel('Number of reads')
     plt.xlabel('%GC')
-    # plt.show()
     plt.savefig(result_file('gc_contents.png'))
+    plt.clf()
 
 
-def get_quality_distribution(input):
-    pass
+def combine_two_scores(old, new):
+    if len(old) < len(new):
+        old, new = new, old
+    for i, e in enumerate(new):
+        old[i] = (old[i] + e) * 1.0 / 2
+    return old
 
+
+def score_to_probabilites(scores):
+    return list(10 ** ((-score) * 1.0 / 10 + 2) for score in scores)
+
+
+def get_quality_distribution(records):
+    qualities = map(get_record_quality, records)
+    mean_scores = reduce(combine_two_scores, qualities, [])
+    probabilities = score_to_probabilites(mean_scores)
+    plt.plot(range(0, len(probabilities)), probabilities)
+    plt.ylabel('Error probability %')
+    plt.xlabel('Base')
+    plt.savefig(result_file('nucleotide_error_probability.png'))
+    plt.clf()
 
 if __name__ == '__main__':
     make_result_dir()
@@ -82,6 +104,7 @@ if __name__ == '__main__':
         with open(filename) as input_file:
             records = list(get_records(input_file))
             get_gc_content(records)
+            get_quality_distribution(records)
 
 
 
